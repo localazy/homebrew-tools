c136b98ac1cd29bed2aa2c3ae018eea6d302df8b
Jan Bílek <honza@elision.design>
2020-07-27 13:24:37 +0200

    Add curl dependency

--- localazy.rb
+++ localazy.rb
@@ -3,6 +3,7 @@ class Localazy < Formula
   homepage "https://localazy.com"
   url "https://dist.localazy.com/macos/macos-v0.9.0-beta.1.tar.gz"
   sha256 "40502061a611435773e1043e639dbe3f3ba5cacaafd75af9561d9910d97978b7"
+  depends_on "curl"
 
   bottle :unneeded
 
