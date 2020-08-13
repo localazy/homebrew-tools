951e4b03030f7163d048b27563e8043f7b1d0fd5
Jan Bílek <honza@elision.design>
2020-08-13 16:57:07 +0200

    Update localazy.rb

--- localazy.rb
+++ localazy.rb
@@ -1,8 +1,8 @@
 class Localazy < Formula
   desc "CLI tool for Localazy platform"
   homepage "https://localazy.com"
-  url "https://dist.localazy.com/macos/macos-1.0.0.tar.gz"
-  sha256 "f2fe9304f976e2359745ff2560ca1e7f35026a4422cf9284243bee25412823d3"
+  url "https://dist.localazy.com/macos/macos-v1.0.1.tar.gz"
+  sha256 "6032e3940f4837431dc4e630f291c54569db8eadf5149d66c2cd8d2f478e169e"
   depends_on "curl"
 
   bottle :unneeded
